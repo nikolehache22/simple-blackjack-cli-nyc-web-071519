9d1869a2cb27ef6af7d8660530c500d5178dfa53
Nikole <nh131215@gmail.com>
2019-07-23 17:19:47 -0400

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -15,22 +15,30 @@ def prompt_user
 end
 
 def get_user_input
-  gets.chomp
+  gets.chomp.strip
 end
 
 def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-def initial_round(card_total)
-  def
-    def
-  sum = deal_card + deal_card 
-  card_total = display_card_total
+def initial_round
+  first_round = deal_card + deal_card 
+  display_card_total(first_round)
+  return first_round
 end
 
-def hit?(number)
-  # code hit? here
+def hit?(card_total)
+  prompt_user 
+  user_input = get_user_input
+  if user_input == "h"
+    card_total += deal_card
+  elsif user_input == "s"
+    card_total
+  else 
+    invalid_command
+    hit?(card_total)
+  end
 end
 
 def invalid_command
@@ -42,6 +50,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
     
